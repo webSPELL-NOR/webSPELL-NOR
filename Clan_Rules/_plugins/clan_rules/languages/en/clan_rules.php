285e5e1c46aabf78799e1ab9af6eff380774de91
T-Seven / DesignPerformance <t-seven@die-irre-truppe.de>
2016-08-09 10:43:56 +0200

    Add new file

---
+++ Clan_Rules/_plugins/clan_rules/languages/en/clan_rules.php
@@ -0,0 +1,37 @@
+<?php
+/*
+##########################################################################
+#                                                                        #
+#           Version 4       /                        /   /               #
+#          -----------__---/__---__------__----__---/---/-               #
+#           | /| /  /___) /   ) (_ `   /   ) /___) /   /                 #
+#          _|/_|/__(___ _(___/_(__)___/___/_(___ _/___/___               #
+#                       Free Content / Management System                 #
+#                                   /                                    #
+#                                                                        #
+#                                                                        #
+#   Copyright 2005-2015 by webspell.org                                  #
+#                                                                        #
+#   visit webSPELL.org, webspell.info to get webSPELL for free           #
+#   - Script runs under the GNU GENERAL PUBLIC LICENSE                   #
+#   - It's NOT allowed to remove this copyright-tag                      #
+#   -- http://www.fsf.org/licensing/licenses/gpl.html                    #
+#                                                                        #
+#   Code based on WebSPELL Clanpackage (Michael Gruber - webspell.at),   #
+#   Far Development by Development Team - webspell.org                   #
+#                                                                        #
+#   visit webspell.org                                                   #
+#                                                                        #
+##########################################################################
+*/
+
+$language_array = array(
+
+/* do not edit above this line */
+	'del_install'=>'<h3>Info</h3><div class="alert alert-danger"><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+  <span class="sr-only">Error:</span>Please remove the file "install-clan_rules.php" from your webspace first!</div>',
+	'clan_rules'=>'clan rules',
+	'new_rules' => 'new rules',
+   'no_clan_rules' => 'no clanrules'
+    );
+
