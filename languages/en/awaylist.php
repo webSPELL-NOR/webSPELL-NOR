100ee8ad15fd56e9be0b651856bb53b88c9d0f48
T-Seven <t-seven@die-irre-truppe.de>
2016-09-24 16:42:14 +0200

    update

---
+++ languages/en/awaylist.php
@@ -0,0 +1,31 @@
+<?php
+$language_array = Array(
+
+    'title_awaylist'=>'Awaylist',
+	'no_reason'=>'you added no reason',
+	'no_date'=>'Please insert a correct date from the calendar',
+	'back'=>'<< back',
+	'clanmember'=>'clanmember',
+	'communitymember'=>'community member',
+	'new_absence'=>'new absence',
+	'message1'=>'<html><head><title>Awaylist Notifier</title></head><body>Hello, one of your ',
+	'message2'=>' has announced a new absence.<br>More information in your Webspell-Admincenter.<br><br><br>This mail was send automatically, please do not reply.<br><br><br><br><div align="right" class="style2 style1">&copy; 2016 - <a href="https://webspell-nor.de/">webSPELL NOR</a> - <br>
+Awaylist Plugin 1.0 by <a href="http://link-coming.com">Urkgrim</a>&nbsp;</div></body></html>',
+	'submitted'=>'Your absence was submitted successfully',
+	'delete'=>'delete',
+	'clanmember_only'=>'sry clanmember only',
+	'login_first'=>'You have to be logged in to show this site!',
+	'register_now'=>'register now',
+	'login'=>'login',
+	'awayfrom'=>'away from:',
+	'awayto'=>'away to:&nbsp;&nbsp;&nbsp;&nbsp;',
+	'reason'=>'reason:&nbsp;',
+	'send'=>'send',
+	'name'=>'name',
+	'from'=>'from',
+	'to'=>'to',
+	'next_absences'=>'next absences',
+	'choose_date'=>'Please choose date',
+
+);
+?>
---
+++ languages/en/carousel.php
@@ -0,0 +1,60 @@
+<?php
+/*
+##########################################################################
+#                                                                        #
+#           Version 4       /                        /   /               #
+#          -----------__---/__---__------__----__---/---/-               #
+#           | /| /  /___) /   ) (_ `   /   ) /___) /   /                 #
+#          _|/_|/__(___ _(___/_(__)___/___/_(___ _/___/___               #
+#                       Free Content / Management System                 #
+#                                   /                                    #
+#                                                                        #
+#                                                                        #
+#   Copyright 2005-2015 by webspell.org                                  #
+#                                                                        #
+#   visit webSPELL.org, webspell.info to get webSPELL for free           #
+#   - Script runs under the GNU GENERAL PUBLIC LICENSE                   #
+#   - It's NOT allowed to remove this copyright-tag                      #
+#   -- http://www.fsf.org/licensing/licenses/gpl.html                    #
+#                                                                        #
+#   Code based on WebSPELL Clanpackage (Michael Gruber - webspell.at),   #
+#   Far Development by Development Team - webspell.org                   #
+#                                                                        #
+#   visit webspell.org                                                   #
+#                                                                        #
+##########################################################################
+*/
+
+$language_array = Array(
+
+/* do not edit above this line */
+
+  'access_denied'=>'Access denied',
+  'actions'=>'Actions',
+  'add_carousel'=>'add Carousel',
+  'back'=>'back',
+  'carousel_pic'=>'Carousel Picture',
+  'banner_to_big'=>'The banner is to big',
+  'carousel_upload_info'=>'Carousel Picture Upload',
+  'current_pic'=>'Current Carousel Picture',
+  'delete'=>'delete',
+  'description'=>'Description',
+  'edit'=>'edit',
+  'edit_carousel'=>'edit Carousel',
+  'format_incorrect'=>'The format of the icon was incorrect. Please upload only banner in *.gif, *.jpg and *.png format.',
+  'is_displayed'=>'Is displayed?',
+  'new_carousel'=>'new Carousel',
+  'no'=>'No',
+  'no_upload'=>'No Image uploaded',
+  'no_entries'=>'No entries',
+  'really_delete'=>'Really delete this Entry?',
+  'sort'=>'Sort',
+  'carousel'=>'Carousel',
+  'carousel_link'=>'Carousel link',
+  'carousels'=>'Carousels',
+  'title'=>'Title',
+  'transaction_invalid'=>'Transaction ID invalid',
+  'to_sort'=>'sort',
+  'yes'=>'Yes'
+);
+
